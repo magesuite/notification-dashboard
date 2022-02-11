df8b45753defc5d4caf64acf199d77efb6ed0ea6
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2022-02-11 08:14:33 +0100

    Initial commit

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageSuite_NotificationDashboard',
+    __DIR__
+);
+
