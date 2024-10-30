a278418b3269cb80140663db1c10e55f00b65496
Paweł Ledwig <pawel.ledwig@creativestyle.pl>
2024-10-30 13:51:47 +0000

    feat: [RK-1686] Email subject per env

--- Service/NotificationSender/Channel/Email.php
+++ Service/NotificationSender/Channel/Email.php
@@ -4,15 +4,12 @@ namespace MageSuite\NotificationDashboard\Service\NotificationSender\Channel;
 
 class Email
 {
-    const EMAIL_TEMPLATE_IDENTIFIER = 'notification';
+    public const EMAIL_TEMPLATE_IDENTIFIER = 'notification';
 
     protected \MageSuite\NotificationDashboard\Model\Command\Notification\AddRawDataToMessage $addRawDataToMessage;
-
-    protected \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation;
-
-    protected \Magento\Framework\Mail\Template\TransportBuilderFactory $transportBuilderFactory;
-
     protected \MageSuite\NotificationDashboard\Helper\Configuration $configuration;
+    protected \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation;
+    protected \Magento\Framework\Mail\Template\TransportBuilderFactory $transportBuilderFactory;
 
     public function __construct(
         \MageSuite\NotificationDashboard\Model\Command\Notification\AddRawDataToMessage $addRawDataToMessage,
@@ -26,17 +23,18 @@ class Email
         $this->configuration = $configuration;
     }
 
-    public function send($notification, $channelsData)
+    public function send(\MageSuite\NotificationDashboard\Api\Data\NotificationInterface $notification, array $channelsData): void
     {
         $this->inlineTranslation->suspend();
 
-        $message = $this->addRawDataToMessage->execute($notification);
+        $emailSubject = $this->getEmailTitle($notification);
+        $emailContent = $this->getEmailContent($notification);
 
         $transport = $this->transportBuilderFactory
             ->create()
             ->setTemplateIdentifier(self::EMAIL_TEMPLATE_IDENTIFIER)
             ->setTemplateOptions(['area' => 'adminhtml', 'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID])
-            ->setTemplateVars(['message' => (string)$message, 'title' => (string)$notification->getTitle()])
+            ->setTemplateVars(['message' => $emailContent, 'title' => $emailSubject])
             ->setFromByScope($this->configuration->getEmailSenderInfo())
             ->addTo($channelsData[0]->getChannel());
 
@@ -47,5 +45,18 @@ class Email
 
         $transport->getTransport()->sendMessage();
         $this->inlineTranslation->resume();
+    }
+
+    protected function getEmailTitle(\MageSuite\NotificationDashboard\Api\Data\NotificationInterface $notification): string
+    {
+        $subjectPrefix = $this->configuration->getEmailSubjectPrefix();
+        $notificationTitle = (string)$notification->getTitle();
+
+        return sprintf('%s%s', $subjectPrefix, $notificationTitle);
+    }
+
+    protected function getEmailContent(\MageSuite\NotificationDashboard\Api\Data\NotificationInterface $notification): string
+    {
+        return (string)$this->addRawDataToMessage->execute($notification);
     }
 }
